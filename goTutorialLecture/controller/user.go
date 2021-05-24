4e5a1252f8b9a46eb704a6c99b3c45dc6429641e
irrfan13 <irfan.khan@enquero.com>
2021-05-24 09:40:20 +0530

    test file changes

--- goTutorialLecture/controller/user.go
+++ goTutorialLecture/controller/user.go
@@ -8,44 +8,46 @@ import (
 	"net/http"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
-
 	"github.com/goTutorialLecture/config/db"
 	"github.com/goTutorialLecture/model"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get user function created successfully")
-	w.Header().Add("content-type", "application/json")
-	var result []model.User
-	db, err := db.GetDBCollection()
+	w.Header().Add("content-type", "application/json") //header
+	var result []model.User                            //model
+	db, err := db.GetDBCollection()                    //database connection
+	// error handling
 	if err != nil {
 		log.Fatal(err)
 	}
+	// collection
 	collection := db.Collection("users")
-
+	// context timeout
 	ctx, _ := context.WithTimeout(context.TODO(), 50*time.Second)
-
+	// mongo method
 	response, err := collection.Find(context.TODO(), bson.M{})
-
+	// error handling
 	if err != nil {
 		log.Fatal("err")
 	}
+	// close the context connection
 	defer response.Close(ctx)
-
+	// decoding for getting the data from database
 	for response.Next(context.Background()) {
 		var users model.User
 		fmt.Println("Result")
 		response.Decode(&users)
 		result = append(result, users)
 	}
-
+	// error handling for all sort of api type
 	if err := response.Err(); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(`{"message":"` + err.Error() + `"}`))
 		return
 	}
-
+	// encoding for every api return response
 	json.NewEncoder(w).Encode(result)
 
 }
--- goTutorialLecture/controller/user_test.go
+++ goTutorialLecture/controller/user_test.go
@@ -18,7 +18,7 @@ func TestGetUser(t *testing.T) {
 }
 
 func TestGetProfile(t *testing.T) {
-	res, err := http.Get("http://localhost:8080/user")
+	res, err := http.Get("http://localhost:8080/profile")
 	fmt.Println("profile testing", res.StatusCode)
 	if http.StatusOK != res.StatusCode {
 		t.Errorf("Expected response code %d. got %d\n", http.StatusOK, res.StatusCode)
